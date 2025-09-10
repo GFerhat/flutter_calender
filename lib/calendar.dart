dd10d6cffa7b27eaa98f56a1b768bc810d325cd1
Ferhat Guemues <ferhat_gumus@teilnehmer.cdemy.de>
2025-09-10 10:58:30 +0200

    prep for the sceleton.

---
+++ lib/calendar.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Calendar extends StatelessWidget {
+  const Calendar({super.key});
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
