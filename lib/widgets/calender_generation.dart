7226db678bcbb1660f5ea3fd6730ea9b7682034d
Ferhat Guemues <ferhat_gumus@teilnehmer.cdemy.de>
2025-09-11 16:54:18 +0200

    Refactor calendar generation to encapsulate header creation in a dedicated method for improved readability and maintainability

--- lib/widgets/calender_generation.dart
+++ lib/widgets/calender_generation.dart
@@ -10,6 +10,20 @@ class TableGenerator extends StatelessWidget {
     int daysOfMonth = DateTime(date.year, date.month + 1, 0).day;
     int daysOfPrevMonth = DateTime(date.year, date.month, 0).day;
 
+    TableRow generateCalendarHead() {
+      List<String> weekdays = ["CW", "MO", "TU", "WE", "TH", "FR", "ST", "SU"];
+      List<TableCell> calendarHeader = [];
+      for (int i = 0; i < weekdays.length; i++) {
+        calendarHeader.add(
+          TableCell(child: Container(child: Text(weekdays[i]))),
+        );
+      }
+      return TableRow(
+        decoration: BoxDecoration(color: Color.fromARGB(255, 212, 198, 198)),
+        children: calendarHeader,
+      );
+    }
+
     TableRow generateWeek(List<TableCell> day) {
       return TableRow(children: day);
     }
@@ -27,14 +41,19 @@ class TableGenerator extends StatelessWidget {
       return TableCell(child: Text(cellContent.toString()));
     }
 
-    return Table(
-      children: <TableRow>[
-        for (int weekCounter = 0; weekCounter < 6; weekCounter++)
-          generateWeek([
-            for (int dayCounter = 0; dayCounter <= 7; dayCounter++)
-              generateDay(dayCounter, weekCounter),
-          ]),
-      ],
+    return Container(
+      padding: EdgeInsets.all(20.0),
+      decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+      child: Table(
+        children: <TableRow>[
+          generateCalendarHead(),
+          for (int weekCounter = 0; weekCounter < 6; weekCounter++)
+            generateWeek([
+              for (int dayCounter = 0; dayCounter <= 7; dayCounter++)
+                generateDay(dayCounter, weekCounter),
+            ]),
+        ],
+      ),
     );
   }
 }
