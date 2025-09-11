82d0888d3bbe94ccff3f137362773c55ceede348
Ferhat Guemues <ferhat_gumus@teilnehmer.cdemy.de>
2025-09-11 13:38:20 +0200

    Update calendar initialization to use current date and enhance table generation logic with calendarweek handling

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -6,7 +6,7 @@ import 'package:flutter_calender/widgets/headsection.dart';
 
 class Calendar extends StatelessWidget {
   Calendar({super.key});
-  DateTime date = DateTime(2025, 11, 9);
+  DateTime date = DateTime.now();
   // DateTime get localDateTime => date;
 
   Widget build(BuildContext context) {
