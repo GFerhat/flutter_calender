02e37391072b85ec2d9208f3f7eb2c705665585d
Ferhat Guemues <ferhat_gumus@teilnehmer.cdemy.de>
2025-09-11 15:11:23 +0200

    Refactor calendar logic to use current date and simplify week generation; remove unused week and day widgets

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -6,11 +6,10 @@ import 'package:flutter_calender/widgets/headsection.dart';
 
 class Calendar extends StatelessWidget {
   Calendar({super.key});
-  DateTime date = DateTime.now();
+  DateTime date = DateTime(2025, 10, 1);
   // DateTime get localDateTime => date;
 
   Widget build(BuildContext context) {
-    print(date.weekday);
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: const Size.fromHeight(56.0), // or your desired height
--- lib/widgets/calender_generation.dart
+++ lib/widgets/calender_generation.dart
@@ -9,29 +9,27 @@ class TableGenerator extends StatelessWidget {
     DateTime firstOfMonth = DateTime(date.year, date.month, 1);
     int daysOfMonth = DateTime(date.year, date.month + 1, 0).day;
     int daysOfPrevMonth = DateTime(date.year, date.month, 0).day;
-    Week currentWeek = Week.current();
-    String currentWeekString = currentWeek.toString().substring(5, 7);
-    int calenderWeek = int.parse(currentWeekString);
-    TableRow generateWeek(List<TableCell> day, int weekCounter) {
-      if (weekCounter == 0) {
-        return TableRow(children: Text(calendarWeek.toString()));
-      } else {
-        return TableRow(children: day);
-      }
+
+    TableRow generateWeek(List<TableCell> day) {
+      return TableRow(children: day);
     }
 
     TableCell generateDay(int dayCounter, int weekCounter) {
-      
       int cellContent = weekCounter * 7 + dayCounter - firstOfMonth.weekday + 1;
       if (cellContent > daysOfMonth) cellContent = cellContent - daysOfMonth;
       if (cellContent < 1) cellContent = cellContent + daysOfPrevMonth;
+      if (dayCounter == 0) {
+        Week currentWeek = Week.fromDate(date);
+        String currentWeekString = currentWeek.toString().substring(5, 7);
+        int calendarWeek = int.parse(currentWeekString);
+        cellContent = calendarWeek + weekCounter;
+      }
       return TableCell(child: Text(cellContent.toString()));
     }
 
-    int weekCounter = 0;
     return Table(
       children: <TableRow>[
-        for (weekCounter = 0; weekCounter < 6; weekCounter++)
+        for (int weekCounter = 0; weekCounter < 6; weekCounter++)
           generateWeek([
             for (int dayCounter = 0; dayCounter <= 7; dayCounter++)
               generateDay(dayCounter, weekCounter),
